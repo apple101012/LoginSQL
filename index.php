54dfc7f45e7f5ab6dea6e5435c6f2a85d2a7c91b
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-08-13 17:56:50 -0400

    omw to success

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <nav>
+        <div class="main">Blogs</div>
+        <ul>
+
+            <li><a href="index.php">Home</a></li>
+            <li><a href="test.php">Test</a></li>
+            <li><a href="signup.php">Sign up</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+    <div class="wrap">
+        <div class="mid">Hey I have text</div>
+    </div>
+</body>
+
+</html>
