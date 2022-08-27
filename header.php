7bf376dfd33b7ed7c6dacec772917e5e2bebcfed
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-08-27 12:40:08 -0400

    setup logout

--- header.php
+++ header.php
@@ -1,10 +1,21 @@
+<?php
+session_start();
+?>
 <nav>
     <div class="main">Blogs</div>
     <ul>
 
         <li><a href="index.php">Home</a></li>
         <li><a href="test.php">Test</a></li>
-        <li><a href="signup.php">Sign up</a></li>
-        <li><a href="login.php">Login</a></li>
+        <?php
+        if (isset($_SESSION["useruid"])) {
+            echo '<li><a href="profile.php">Profile page</a></li>';
+            echo '<li><a href="inc/logout.inc.php">Log out</a></li>';
+        } else {
+            echo '<li><a href="signup.php">Sign up</a></li>';
+            echo '<li><a href="login.php">Login</a></li>';
+        }
+        ?>
+
     </ul>
 </nav>
